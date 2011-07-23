4b7e36b16720bd9da62ec2d9e79df4355d9ebba3
Sascha Wessel <swessel@gr4yweb.de>
2011-07-23 13:28:04 +0200

    optimized readability of the code and fixed the memory leak.

--- LocateMyMac/LocateMyMacAppDelegate.m
+++ LocateMyMac/LocateMyMacAppDelegate.m
@@ -25,20 +25,39 @@
            fromLocation:(CLLocation *)oldLocation
 {
     CLLocationCoordinate2D coordinate = [newLocation coordinate];
+    
     NSString *longitudeString = [NSString stringWithFormat:@"%f", coordinate.longitude];
     NSString *latitudeString = [NSString stringWithFormat:@"%f", coordinate.latitude];
+    NSString *coordinateString = [NSString stringWithFormat:@"%@,%@", latitudeString, longitudeString];
+    
     [longitudeField setStringValue:longitudeString];
     [latitudeField setStringValue:latitudeString];    
-    [coordinateField setStringValue:[NSString stringWithFormat:@"%@,%@", latitudeString, longitudeString]];
+    [coordinateField setStringValue:coordinateString];
     [coordinateField setHidden:NO];
     [locationManager stopUpdatingLocation];
+    
+    [latitudeString release];
+    [longitudeString release];
+    [coordinateString release];
+}
+
+- (void)locationManager:(CLLocationManager *)manager 
+       didFailWithError:(NSError *)error 
+{
+    NSAlert *alert = [NSAlert alertWithMessageText:@"CoreLocation Error"
+                                     defaultButton:@"OK" alternateButton: nil otherButton:nil
+                        informativeTextWithFormat:[error description]];
+    [alert beginSheetModalForWindow:window modalDelegate:self didEndSelector:nil contextInfo:nil];
 }
 
 - (IBAction)locateMe:(id)sender {
+    [latitudeField setStringValue:@""];
+    [longitudeField setStringValue:@""];
     [locationManager startUpdatingLocation];
 }
 
 - (void)dealloc {
+    [locationManager setDelegate:nil];
     [locationManager release];
     [super dealloc];
 }
